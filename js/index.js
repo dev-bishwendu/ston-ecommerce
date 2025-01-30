b8a4e3fe1a5d4e716459304fded94bc70031e5eb
AYON <sarkerbishwendu90@gmail.com>
2025-01-30 20:26:22 +0600

    sb banner with responsive

--- js/index.js
+++ js/index.js
@@ -7,6 +7,16 @@ var swiper = new Swiper(".mySwiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+      200: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 50,
+      },
+    },
     pagination: {
       el: ".swiper-pagination",
     },
