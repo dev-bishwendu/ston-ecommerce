e662494be1ebd4f32d57e9fa7d1f144fe48fa00f
AYON <sarkerbishwendu90@gmail.com>
2025-01-30 14:26:57 +0600

    collection section

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 50,
+    loop: true,
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
